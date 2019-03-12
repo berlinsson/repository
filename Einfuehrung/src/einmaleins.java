6affc21a1b379b9d360d63f1026d0f6ce07da308
mathis.steffan <mathis.steffan@r210-205.schule.local>
2019-03-12 11:27:07 +0100

    einmaleins

---
+++ Einfuehrung/src/einmaleins.java
@@ -0,0 +1,20 @@
+
+public class einmaleins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		moin();
+	}
+	
+	public static void moin() 
+	{
+		for (int zaehler1=1; zaehler1 <=10; zaehler1++){
+			for (int zaehler2=1; zaehler2 <=10; zaehler2++){
+			System.out.print(zaehler2*zaehler1+" \t" );
+			}
+			System.out.println();
+			} 
+	     
+	    }
+}
